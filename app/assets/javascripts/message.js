857b784979d6b5d0803780abde3d0b607d1aebf5
accttakaya <takaya.satou.0206@gmail.com>
2020-02-12 00:46:24 +0900

    done
    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,40 @@
 $(function(){
+
   function buildHTML(message){
-    if(message.image){
+    if (message.content && message.image) {
+      var html = 
+       `<div class="message-box"  data-message-id=${message.id}>
+          <div class="message-box__data">
+            <div class="message-box__data__whose">
+              ${message.user_name}
+            </div>
+            <div class="message-box__data__when">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-box__text">
+            ${message.content}
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else if (message.content) {
+      var html =
+        `<div class="message-box"  data-message-id=${message.id}>
+           <div class="message-box__data">
+             <div class="message-box__data__whose">
+               ${message.user_name}
+             </div>
+             <div class="message-box__data__when">
+               ${message.created_at}
+             </div>
+           </div>
+           <div class="message-box__text">
+             ${message.content}
+           </div>
+         </div>` 
+      return html;
+    } else if (message.image) {
       var html =
        `<div class="message-box"  data-message-id=${message.id}>
           <div class="message-box__data">
@@ -15,7 +49,7 @@ $(function(){
             ${message.content}
           </div>
           <img src=${message.image} >
-       </div>`
+        </div>`
       return html;
     } else {
       var html =
@@ -61,5 +95,30 @@ $(function(){
      .always(function(){
       $('.send-button').prop('disabled', false);
      });
-  })
+  });
+  var reloadMessages = function() {
+    last_message_id = $('.message-box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages,7000);
+  }
 });
