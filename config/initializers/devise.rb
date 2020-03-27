9ec1f5abd3dd45e4ddab90a8535d15abe898a1ee
accttakaya <takaya.satou.0206@gmail.com>
2020-03-28 00:05:55 +0900

    config.secret_keyのコメントアウトを外す

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '6cc51011f384453a8c294f594b4cd5333adbc5db39d36a63d6c5a564100922812e48fe39420aa1bbc82641aa5310d04b18e27171812604b9df49cf0ed1c46312'
+  config.secret_key = '6cc51011f384453a8c294f594b4cd5333adbc5db39d36a63d6c5a564100922812e48fe39420aa1bbc82641aa5310d04b18e27171812604b9df49cf0ed1c46312'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
